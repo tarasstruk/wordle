0694ce511478d5ffc2decf06b89a9e0094ecac3c
Taras <t.struk@sonnen.de>
2023-08-16 09:20:45 +0200

    process feedback 1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::process;
 
 mod verifier;
-use verifier::Verifier;
+use verifier::{GuessResult, Verifier};
 
 fn main() {
     println!("Wordle Game");
@@ -13,29 +13,27 @@ fn main() {
             let mut buf = String::new();
             io::stdin().read_line(&mut buf).unwrap();
             let input = buf.trim();
+
             if input == "exit" {
                 process::exit(0);
             }
             if input == "restart" {
                 break;
             }
-
             if input.len() != 4 {
                 println!("Please enter a 4-characters word");
                 continue;
             }
 
-            if service.verify(input) {
-                println!("Congratulations, you have guessed the word!");
-                break;
-            } else {
-                println!("{}\n", service.hint());
+            match service.verify(input) {
+                GuessResult::Guessed => {
+                    println!("Congratulations, you have guessed the word!");
+                    break;
+                }
+                GuessResult::Missed { hint } => {
+                    println!("{}\n", hint);
+                }
             }
         }
     }
-}
-
-#[cfg(test)]
-mod tests {
-    // use super::*;
 }
